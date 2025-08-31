54ec4d22cd3a595669fb54fdf32947fca32adfc1
kuldeep2040 <kuldeepsinghks2040@gmail.com>
2025-08-31 23:12:59 +0530

    added the music deployed url

--- main/vite.config.js
+++ main/vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, '.', '')
-  
-  return {
+export default defineConfig({
   plugins: [
     react(),
     federation({
       name: 'main',
       remotes: {
-        music: env.VITE_MUSIC_REMOTE_URL
+        music: 'https://finac-plus-intern-assignment.vercel.app/assets/remoteEntry.js'
       },
       shared: {
         'react': {
@@ -35,6 +31,5 @@ export default defineConfig(({ mode }) => {
   },
   preview: {
     port: 3000
-  }
   }
 })
